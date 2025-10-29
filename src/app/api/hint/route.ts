7a0696ecc9c905fbc2988b961b1938892a062f2b
shuntkashiwa-cloud <shunt.kashiwa@gmail.com>
2025-10-29 21:36:11 +0900

    test

--- src/app/api/hint/route.ts
+++ src/app/api/hint/route.ts
@@ -4,9 +4,9 @@ export async function POST(request: Request) {
     const data = await request.json();
     const numquiz = data.numquiz;
     const numhints = data.numhints;
-    if (!process.env.HINTS) {
+    if (!process.env.HINT) {
         return NextResponse.json({ hint: "" });
     }else{
-        return NextResponse.json({ hint: process.env.HINTS.split("/")[numquiz].split(".")[numhints] ,saidai:process.env.HINTS.split("/")[numquiz].split(".").length});
+        return NextResponse.json({ hint: process.env.HINT.split("/")[numquiz].split(".")[numhints] ,saidai:process.env.HINT.split("/")[numquiz].split(".").length});
     }
 }
